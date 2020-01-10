b368b06910805a581704bdee2245c09859cc5845
LZHS <1050629507@QQ.COM>
2020-01-10 23:55:18 +0800

    :rotating_light: 脚本自动提交，提交时间2020-01-10 23:55:16

--- lib/components/pages/main/persion_center/persion_center_page.dart
+++ lib/components/pages/main/persion_center/persion_center_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo/common/common_utils.dart';
 import 'package:flutter_demo/common/widgets/divider_widget.dart';
 import 'package:flutter_demo/components/pages/main/persion_center/bean/item_data.dart';
 import 'package:flutter_demo/config/resources.dart';
@@ -65,6 +66,82 @@ class _PersionCenterPageState extends State<PersionCenterPage> {
                         "lib/assets/images/persion/icon_persion_head.jpg"),
                     fit: BoxFit.cover),
               ),
+              child: Stack(
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: InkWell(
+                      onTap: () {
+                        Log.d("你点击了消息提醒");
+                      },
+                      child: Container(
+                        width: 50.0,
+                        height: 50.0,
+                        margin: EdgeInsets.only(top: 20.0),
+                        alignment: Alignment.center,
+                        child: Image(
+                          width: 20.0,
+                          height: 20.0,
+                          fit: BoxFit.scaleDown,
+                          image: AssetImage(
+                              "lib/assets/images/persion/icon_hint.png"),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: InkWell(
+                      onTap: () {
+                        Log.d("你点击了 用户 信息");
+                      },
+                      child:Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Container(
+                              width: 56.0,
+                              height: 56.0,
+                              margin: EdgeInsets.only(left: 20.0),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.rectangle,
+                                borderRadius: BorderRadius.circular(28.0),
+                                image: DecorationImage(
+                                  image: AssetImage(
+                                    "lib/assets/images/persion/icon_persion_test_head.jpeg",
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 10.0),
+                              child: Text(
+                                "xiaopiu_😋",
+                                style: TextStyle(
+                                    fontSize: 18.0, color: Colors.white),
+                              ),
+                            )
+                          ],
+                        )
+                      ,
+                      Padding(
+                        padding: EdgeInsets.only(right:20.0),
+                        child: Image(
+                          width: 14.0,
+                          height: 14.0,
+                          fit: BoxFit.fill,
+                          image: AssetImage("lib/assets/images/persion/icon_persion_head_back.png"),
+                        ),
+                      )
+                      ],
+                    ),
+                  ),
+                  ),
+                ],
+              ),
             ),
             createItemWidgets(),
             Container(
@@ -82,7 +159,7 @@ class _PersionCenterPageState extends State<PersionCenterPage> {
     );
   }
 
-  createItemWidgets() { 
+  createItemWidgets() {
     return Container(
       width: double.infinity,
       child: Column(
@@ -105,72 +182,96 @@ class _PersionCenterPageState extends State<PersionCenterPage> {
   /// 创建单个 Item  控件
   ///```
   createItem(ItemData item) {
-    
-// TODO: 这里将要实现按钮的单击事件以及 单机效果
-    return Container(
+    return Material(
       color: Colors.white,
-      padding: EdgeInsets.only(left: 15.0),
-      child: Column(
-        children: <Widget>[
-          Container(
-            height: 50.0,
-            width: double.infinity,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Row(
+      child: InkWell(
+        splashColor: Color(0x55B2B2B2),
+        highlightColor: Color(0x22D4D4D4),
+        radius: 375.0,
+        onTap: () => clickItem(item),
+        child: Container(
+          padding: EdgeInsets.only(left: 15.0),
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 50.0,
+                width: double.infinity,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    Image(
-                      width: 20.0,
-                      height: 20.0,
-                      fit: BoxFit.scaleDown,
-                      image: AssetImage(item.icon),
+                    Row(
+                      children: <Widget>[
+                        Image(
+                          width: 20.0,
+                          height: 20.0,
+                          fit: BoxFit.scaleDown,
+                          image: AssetImage(item.icon),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(left: 15.0),
+                          child: Text(
+                            item.title,
+                            style: TextStyle(
+                              color: Color(0xff383838),
+                              fontSize: 14.0,
+                            ),
+                          ),
+                        )
+                      ],
                     ),
                     Padding(
-                      padding: EdgeInsets.only(left: 15.0),
-                      child: Text(
-                        item.title,
-                        style: TextStyle(
-                          color: Color(0xff383838),
-                          fontSize: 14.0,
-                        ),
+                      padding: EdgeInsets.only(right: 18.0),
+                      child: Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(right: 5.0),
+                            child: item.subTitle == ""
+                                ? Container()
+                                : Text(
+                                    item.subTitle,
+                                    style: TextStyle(
+                                      fontSize: 14.0,
+                                      color: Color(0xffa6a6a6),
+                                    ),
+                                  ),
+                          ),
+                          Image(
+                            width: 14.0,
+                            height: 14.0,
+                            fit: BoxFit.scaleDown,
+                            image: AssetImage(
+                                "lib/assets/images/persion/icon_persion_back.png"),
+                          )
+                        ],
                       ),
-                    )
+                    ),
                   ],
                 ),
-                Padding(
-                  padding: EdgeInsets.only(right: 18.0),
-                  child: Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.only(right: 5.0),
-                        child: item.subTitle == ""
-                            ? Container()
-                            : Text(
-                                item.subTitle,
-                                style: TextStyle(
-                                  fontSize: 14.0,
-                                  color: Color(0xffa6a6a6),
-                                ),
-                              ),
-                      ),
-                      Image(
-                        width: 14.0,
-                        height: 14.0,
-                        fit: BoxFit.scaleDown,
-                        image: AssetImage(
-                            "lib/assets/images/persion/icon_persion_back.png"),
-                      )
-                    ],
-                  ),
-                ),
-              ],
-            ),
+              ),
+              item.isShowDivi ? DividerWidget() : Container(),
+            ],
           ),
-          item.isShowDivi ? DividerWidget() : Container(),
-        ],
+        ),
       ),
     );
+  }
+
+  clickItem(ItemData item) {
+    switch (item.title) {
+      case "个人资料":
+        break;
+      case "咖啡钱包":
+        break;
+      case "优惠券":
+        break;
+      case "兑换优惠":
+        break;
+      case "发票管理":
+        break;
+      case "帮助反馈":
+        break;
+      default:
+    }
   }
 }
