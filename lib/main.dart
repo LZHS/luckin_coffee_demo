3a19f7fdc2135e9df0860ccce0f813f7abfa62a7
LiuHuaChen <1050629507@QQ.com>
2020-01-15 11:07:05 +0800

    项目整理

--- lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_demo/test_demo/1911/191114/components/app/app_component.dart'; 
-
-void main() {
-  //debugPaintSizeEnabled = true;
-  runApp(AppComponent());
-}
-
--- lib/test_demo/1911/191114/config/application.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_demo/fluro/src/router.dart';
-
-class Application{
-  static Router router;
-}
