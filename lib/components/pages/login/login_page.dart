1eeaf5aafc32502767940fac5510bcdb4f720831
LiuHuaChen <1050629507@QQ.com>
2020-02-27 17:59:07 +0800

    修改 手机号登陆页面

--- lib/components/pages/login/login_page.dart
+++ lib/components/pages/login/login_page.dart
@@ -19,94 +19,104 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       appBar: HeadTitleBar(),
       body: SafeArea(
-        child: Stack(
-          children: <Widget>[
-            Positioned(
-              top: 26.0,
-              left: 0.0,
-              right: 0.0,
-              child: Image.asset(
-                "lib/assets/images/login/icon_login.png",
-                width: 80.0,
-                height: 94.0,
-                fit: BoxFit.contain,
+        child: Material(
+          color: Colors.white,
+          child: Stack(
+            children: <Widget>[
+              Positioned(
+                top: 26.0,
+                left: 0.0,
+                right: 0.0,
+                child: Image.asset(
+                  "lib/assets/images/login/icon_login.png",
+                  width: 80.0,
+                  height: 94.0,
+                  fit: BoxFit.contain,
+                ),
               ),
-            ),
-            Positioned(
-              top: 15.0,
-              bottom: 0.0,
-              left: 0.0,
-              right: 0.0,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    width: double.infinity,
-                    height: 44.0,
-                    margin: EdgeInsets.only(left: 38.0, right: 38.0),
-                    child: Ink(
-                      decoration: BoxDecoration(
-                          color: Color(0xff49c265),
-                          borderRadius: BorderRadius.all(Radius.circular(4.0))),
-                      child: InkResponse(
-                        borderRadius: BorderRadius.all(Radius.circular(4.0)),
-                        splashColor: Color(0xAAC9EEC2),
-                        highlightShape: BoxShape.rectangle,
-                        containedInkWell: true,
-                        onTap: goWeChatLogin,
-                        child: Center(
-                          child: Text(
-                            "微信一键登录",
-                            style: TextStyle(
-                              fontSize: 16.0,
-                              color: Colors.white,
+              Positioned(
+                top: 15.0,
+                bottom: 0.0,
+                left: 0.0,
+                right: 0.0,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: double.infinity,
+                      height: 44.0,
+                      margin: EdgeInsets.only(left: 38.0, right: 38.0),
+                      child: Ink(
+                        decoration: BoxDecoration(
+                            color: Color(0xff49c265),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(4.0))),
+                        child: InkResponse(
+                          borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                          splashColor: Color(0xAAC9EEC2),
+                          highlightShape: BoxShape.rectangle,
+                          containedInkWell: true,
+                          onTap: goWeChatLogin,
+                          child: Center(
+                            child: Text(
+                              "微信一键登录",
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                color: Colors.white,
+                              ),
                             ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  Container(
-                    width: double.infinity,
-                    height: 44.0,
-                    margin: EdgeInsets.only(left: 38.0, right: 38.0, top: 25.0),
-                    child: Ink(
-                      decoration: BoxDecoration(
+                    Container(
+                      width: double.infinity,
+                      height: 44.0,
+                      margin:
+                          EdgeInsets.only(left: 38.0, right: 38.0, top: 25.0),
+                      child: Ink(
+                        decoration: BoxDecoration(
                           color: Colors.transparent,
                           borderRadius: BorderRadius.all(Radius.circular(4.0)),
                           border:
-                              Border.all(color: Color(0xff88afd5), width: 1.0),),
-                      child: InkResponse(
-                        borderRadius: BorderRadius.all(Radius.circular(4.0)),
-                        splashColor: Color(0x9988afd5),
-                        highlightShape: BoxShape.rectangle,
-                        containedInkWell: true,
-                        onTap: goPhoneCode,
-                        child: Center(
-                          child: Text(
-                            "手机验证码登录",
-                            style: TextStyle(
-                              fontSize: 16.0,
-                              color: Color(0xff88afd5),
+                              Border.all(color: Color(0xff88afd5), width: 1.0),
+                        ),
+                        child: InkResponse(
+                          borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                          splashColor: Color(0x9988afd5),
+                          highlightShape: BoxShape.rectangle,
+                          containedInkWell: true,
+                          onTap: goPhoneCode,
+                          child: Center(
+                            child: Text(
+                              "手机验证码登录",
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                color: Color(0xff88afd5),
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  )
-                ],
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
-goPhoneCode(){
-  Log.d("手机验证码登录");
-  Application.router.navigateTo(context, loginPhoneCode+"?isDeficiencyNum=false",transition: TransitionType.inFromLeft);
-}
+
+  goPhoneCode() {
+    Log.d("手机验证码登录");
+    Application.router.navigateTo(
+        context, loginPhoneCode + "?isDeficiencyNum=false",
+        transition: TransitionType.inFromLeft);
+  }
+
   goWeChatLogin() {
     Log.d("跳转至为微信登录页面");
   }
--- lib/components/pages/login/login_phone_page.dart
+++ lib/components/pages/login/login_phone_page.dart
@@ -1,66 +1,151 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_demo/common/common_utils.dart';
+import 'package:flutter_demo/common/widgets/divider_widget.dart';
 import 'package:flutter_demo/common/widgets/head_title_bar.dart';
+import 'package:flutter_demo/config/res/colors.dart';
 
 ///```
 ///手机验证码登录 页面
 ///```
 class LoginPhonePage extends StatefulWidget {
-  bool isDeficiency;
-  LoginPhonePage({String isDeficiencyNum}) {
-    this.isDeficiency = isDeficiencyNum == null;
-  }
+  final String isDeficiencyNum;
+  LoginPhonePage({this.isDeficiencyNum});
   @override
   _LoginPhonePageState createState() => _LoginPhonePageState();
 }
 
 class _LoginPhonePageState extends State<LoginPhonePage> {
+  List chooseAris = [
+    " 中国 ",
+    "+86"
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: HeadTitleBar(),
       body: SafeArea(
-        child: Stack(
-          children: <Widget>[
-            Positioned(
-              top: 26.0,
-              left: 0.0,
-              right: 0.0,
-              child: Image.asset(
-                "lib/assets/images/login/icon_login.png",
+        child: Material(
+          color: Colors.white,
+          child: Stack(
+            alignment: AlignmentDirectional.center,
+            fit: StackFit.expand,
+            overflow: Overflow.clip,
+            children: <Widget>[
+              Positioned(
+                top: 26.0,
                 width: 80.0,
                 height: 94.0,
-                fit: BoxFit.contain,
+                child: Image.asset(
+                  "lib/assets/images/login/icon_login.png",
+                  width: 80.0,
+                  height: 94.0,
+                  fit: BoxFit.contain,
+                ),
               ),
-            ),
-            Positioned(
-              top: 134.0,
-              left: 0.0,
-              right: 0.0,
-              child: Visibility(
-                visible: !this.widget.isDeficiency,
-                child: Center(
-                  child: Text(
-                    "为了您的账号安全，请绑定手机",
-                    style: TextStyle(
-                      color: Color(0xff55799d),
-                      fontSize: 12.0,
+              Positioned(
+                top: 134.0,
+                child: Visibility(
+                  visible: this.widget.isDeficiencyNum == "false",
+                  child: Center(
+                    child: Text(
+                      "为了您的账号安全，请绑定手机",
+                      style: TextStyle(
+                        color: Color(0xff55799d),
+                        fontSize: 12.0,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Positioned(
-              top: 15.0,
-              bottom: 0.0,
-              left: 0.0,
-              right: 0.0,
-              child: Container(
-                /// TODO 手机验证码登录 页面 未完成
-              ),
-            ),
-          ],
+              Positioned(
+                  top: 175.0,
+                  left: 0.0,
+                  right: 0.0,
+                  child: Container(
+                    margin: EdgeInsets.only(left: 38.0, right: 38.0),
+                    child: Column(children: <Widget>[
+                      Container(
+                        height: 60.0,
+                        child: Column(
+                          children: <Widget>[
+                            Expanded(
+                              child: Ink(
+                                child: InkResponse(
+                                  highlightColor: Color(0xfff2f2f2),
+                                  highlightShape: BoxShape.rectangle,
+                                  radius: 0.0,
+                                  onTap: goChooseAri,
+                                  child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: <Widget>[
+                                        Row(children: <Widget>[
+                                          Text(
+                                            "选择国家/地区",
+                                            style: TextStyle(
+                                                color: Color(0xff808080),
+                                                fontSize: 14.0),
+                                          ),
+                                          Padding(
+                                            padding: EdgeInsets.only(left: 5.0),
+                                            child: Text("${chooseAris.elementAt(0)} (${chooseAris.elementAt(1)})",
+                                                style: TextStyle(
+                                                    color: appBarTitleColor,
+                                                    fontSize: 14.0)),
+                                          ),
+                                        ]),
+                                        Image.asset(
+                                          "lib/assets/images/login/icon_login_back.png",
+                                          width: 14.0,
+                                          height: 14.0,
+                                        ),
+                                      ]),
+                                ),
+                              ),
+                            ),
+                            DividerWidget()
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 60.0,
+                        child: Column(
+                          children: <Widget>[
+                            Expanded(
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  labelText: "请输入手机号",
+                                  labelStyle: TextStyle(
+                                      fontSize: 14.0, color: Color(0xffa6a6a6)), 
+
+                                   prefixText: "${chooseAris.elementAt(1)}",
+                                ),
+                                keyboardType: TextInputType.phone,
+                                textInputAction: TextInputAction.next,
+                                maxLines: 1,
+                                maxLength: null,
+                                inputFormatters: <TextInputFormatter>[
+                                  LengthLimitingTextInputFormatter(11)
+                                ], 
+                                // autofocus: true,
+                              ),
+                            ),
+                            DividerWidget(),
+                          ],
+                        ),
+                      ),
+                    ]),
+                  ))
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  goChooseAri() {
+    Log.d("选择国家/地区");
   }
 }
