be66203cec2292a4eb7b64fd75e7b590c2846a6f
LiuHuaChen <1050629507@QQ.com>
2020-05-20 14:43:32 +0800

    修改更換 工作空間

--- lib/common/fluro/router.dart
+++ lib/common/fluro/router.dart
@@ -39,7 +39,7 @@ class Router {
     return _routeTree.matchRoute(path);
   }
 
-  bool pop<T extends Object>(BuildContext context, [ T result ]) => Navigator.pop(context,result);
+  void pop<T extends Object>(BuildContext context, [ T result ]) => Navigator.pop(context,result);
 
   ///
   Future navigateTo(BuildContext context, String path,
