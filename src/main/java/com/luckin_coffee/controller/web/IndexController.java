a3ff5d3db6f72ccb7c0051d8977d65710d041279
LiuHuaChen <1050629507@QQ.com>
2020-10-22 18:12:48 +0800

    :arrow_up: 脚本自动提交，提交时间2020-10-22 18:12:46

--- src/main/java/com/luckin_coffee/controller/web/IndexController.java
+++ src/main/java/com/luckin_coffee/controller/web/IndexController.java
@@ -1,15 +1,19 @@
-package com.luckin_coffee.controller;
+package com.luckin_coffee.controller.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+@RequestMapping("/web")
 public class IndexController {
 
     @RequestMapping(value = "/",method = RequestMethod.GET)
-    public String index(){
-        return "hello, there";
+    public String index(Model model){
+        model.addAttribute("message","IndexController index");
+        return "index";
     }
 
 }
